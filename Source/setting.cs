f0b3605fedc9c0b61379b38fc7b57b3efa431fb4
极光薄荷糖 <kearril0528@outlook.com>
2026-07-27 22:24:55 +0800

    修改项目结构：将source文件夹从1.6移动至根目录

---
+++ Source/setting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace ChooseWhereToLand
+{
+    public class ChooseWhereToLand_Mod : Mod
+    {
+        public static ChooseWhereToLand_Settings settings;
+
+        public ChooseWhereToLand_Mod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<ChooseWhereToLand_Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+
+            listing.CheckboxLabeled(
+                "CWTL_UseCustomLandingSpot".Translate(),
+                ref settings.useCustomLandingSpot
+            );
+
+            listing.End();
+        }
+
+        public override string SettingsCategory() => "CWTL_Setting".Translate();
+    }
+
+    public class ChooseWhereToLand_Settings : ModSettings
+    {
+        
+        public bool useCustomLandingSpot = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref useCustomLandingSpot, "useCustomLandingSpot", true);
+        }
+    }
+}
